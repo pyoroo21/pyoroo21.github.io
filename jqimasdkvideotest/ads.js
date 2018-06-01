30f258517b74218e802c6f09b25519f01dc3a8cb
pyoroo21 <37170900+pyoroo21@users.noreply.github.com>
2018-06-01 17:22:53 +0800

    Update ads.js

--- jqimasdkvideotest/ads.js
+++ jqimasdkvideotest/ads.js
@@ -40,6 +40,7 @@ function setUpIMA() {
   // Request video ads.
   var adsRequest = new google.ima.AdsRequest();
   //adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator='
+  
   adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/27721068/an_video_page/instream_test&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]'
   // Specify the linear and nonlinear slot sizes. This helps the SDK to
   // select the correct creative if multiple are returned.
